5c739ff3d1389687c48e306f1f9f5af77e9ae1bc
Fernando Espinosa Iniguez <fespinosa@ucdavis.edu>
2024-08-04 14:20:38 -0700

    change mobilesam path

--- ClaudeSeg/SegmentFx/python/segmentation.py
+++ ClaudeSeg/SegmentFx/python/segmentation.py
@@ -19,7 +19,7 @@ def process_batch(batch_frames, predictor, user_mask, input_box):
 def load_model(manual=False):
     from mobile_sam import sam_model_registry, SamPredictor, SamAutomaticMaskGenerator
     model_type = "vit_t" # tiny model
-    sam_checkpoint = "../../../MobileSAM/weights/mobile_sam.pth"
+    sam_checkpoint = "../models/mobile_sam.pth"
     device = "cpu"
     # device = "cuda" if torch.cuda.is_available() else "cpu"
 
