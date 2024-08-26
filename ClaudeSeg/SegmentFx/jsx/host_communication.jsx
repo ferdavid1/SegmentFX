040cc342a69cf54fd1737e4fc0fce97f64dc9dc2
Fernando Espinosa Iniguez <fespinosa@ucdavis.edu>
2024-08-26 01:16:46 -0700

    attempting to find where i am

--- ClaudeSeg/SegmentFx/jsx/host_communication.jsx
+++ ClaudeSeg/SegmentFx/jsx/host_communication.jsx
@@ -1,7 +1,7 @@
 // host_communication.jsx
 
 function testExtendScriptFunction() {
-    return "ExtendScript is working!" + File($.fileName).parent.parent.fsName;
+    return "ExtendScript is working!" + File($.fileName).fsName;
 }
 
 // Function to get the current project
